66c41ac9ed9e5351aea01e7ae9fc39719996b362
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-10-05 22:22:07 +0100

    rewrite the code

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = [replace if i == search else i for i in my_list]
-    return new_list
+def uniq_add(my_list=[]):
+    list_set = set(my_list)
+    uniq_sum = 0
+    for item in list_set:
+        uniq_sum += item
+    return uniq_sum
