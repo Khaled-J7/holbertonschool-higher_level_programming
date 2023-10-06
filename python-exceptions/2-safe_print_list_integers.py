06cbd94751c1cf4523468394ac629823e4e690c5
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-10-06 20:28:44 +0100

    Exceptions

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_print = 0
+    for idx in range(x):
+        if x == idx:
+            break
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            num_print += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return num_print
