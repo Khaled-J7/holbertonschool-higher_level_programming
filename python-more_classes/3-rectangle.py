803e16631ea28e640eec109c50f383f2cea5be8c
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-10-13 22:42:30 +0100

    fix checks

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,53 +1,70 @@
 #!/usr/bin/python3
-""" empty class Rectangle that defines a rectangle
-"""
+"""Define the height and width of a rectangle"""
 
 
 class Rectangle:
-    """ class rectangle"""
+    """Defines a rectangle.
+        Private instance attribute: width.
+        Instantiate with optional height.
+    """
     def __init__(self, width=0, height=0):
-        """ Instantiation with optional width and height"""
+        """Initialize the data conditions"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ width
-        """
+        """Retrieve the width."""
         return self.__width
-
-    @property
-    def height(self):
-        """ height
-        """
-        return self.__height
 
     @width.setter
     def width(self, value):
-        """ width setter
-        """
-        if type(value) is not int:
+        """Sets the width to a value."""
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """Retrieve the height."""
+        return self.__height
+
     @height.setter
     def height(self, value):
-        """ height setter
-        """
-        if type(value) is not int:
+        """Sets the height to a value."""
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """ returns rectangle area"""
-        return self.__width * self.__height
+        """
+        Area of a rectangle
+        :return: area of a rectangle
+        """
+        return self.__height * self.__width
 
     def perimeter(self):
-        """ returns rectangle perimiter"""
-        if self.__width is 0 or self.__height is 0:
+        """
+        Perimeter of a rectangle
+        :return: perimeter of a rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return self.__width * 2 + self.__height * 2
+        else:
+            return 2 * self.__height + 2 * self.__width
+
+    def __str__(self):
+        """prints in stdout the area with the character #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        hashes = []
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                hashes.append("#")
+            if i != self.__height - 1:
+                hashes.append("\n")
+        return "".join(hashes)
