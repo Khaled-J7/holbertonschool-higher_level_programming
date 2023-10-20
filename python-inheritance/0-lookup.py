17689daef2e2414535674a0c45676b3333cfda44
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2023-10-20 23:21:22 +0100

    check

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""
+a module for the function lookup
+"""
+
+
+def lookup(obj):
+    """
+    a function that returns the list of available
+    attributes and methods of an object.
+    """
+    return dir(obj)
