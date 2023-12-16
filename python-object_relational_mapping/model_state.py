8a75d4f2c6f03390bc063b1f47d52b02d0e7a1a4
Khaled Jallouli <jallouli.khaled.2001@gmail.com>
2023-12-16 16:51:22 +0100

    Define state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-Python file that contains the class definition of a State and an instance
-"""
-from sqlalchemy import Column, Integer, String
+""" Define State model """
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    State class that inherits from Base
+    """ Define a class State to be linked to db table """
+    __tablename__ = 'states'
 
-    Attributes:
-        id: Id state
-        name: Name of state
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, nullable=False,
+                autoincrement=True, unique=True,
+                primary_key=True)
+
     name = Column(String(128), nullable=False)
